2592b42929f67277c0d7e34708dfdd3eb65d6c69
Peter Moore <masterpm84@gmail.com>
2021-05-06 15:31:05 -0500

    version 1.0.0

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import DashboardConfig from './DashboardConfig';
+//import DashboardConfig from './DashboardConfig';
 
 
 class Dashboard extends Component {
